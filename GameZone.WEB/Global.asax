d336c3e2d4bc4fd922f3c715838b90def1420a71
Bernard Oreva <boreva@funmobilelive.com>
2017-11-22 10:48:20 +0100

    GameZone New Platform First Commit

---
+++ GameZone.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameZone.WEB.MvcApplication" Language="C#" %>
---
+++ GameZone.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameZone.WEB.Startup))]
+namespace GameZone.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
