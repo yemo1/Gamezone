86f17e12de9d74292c22876014c84a6b7a97cbb6
Bernard Oreva <boreva@funmobilelive.com>
2017-11-29 18:47:28 +0100

    Game Image Display on Games Page

---
+++ GameZone.WEB/Scripts/Games/Index.js
@@ -0,0 +1,44 @@
+﻿
+var formTitle = _Title;
+
+gamezoneApp.controller('gamezoneCtrlr', function ($scope, $http) {
+  
+
+    $scope.obj = {};
+    $scope.basicObj = {};
+    $scope.contactObj = {};
+    $scope.rowCollection = [];
+    $scope.CountryList = [{}];
+    $scope.StateList = [];
+    $scope.statusList = [];
+    $scope.branchList = [{}];
+    $scope.modalBranchList = [{}];
+    $scope.employeeList = [{}];
+    $scope.modalHeaderText = "New " + formTitle + " Details";
+    $scope.roleList = [{}];
+    $scope.originalData = [];
+
+    //Get ApplicationUser Data from DB
+    $scope.getGameData = function () {
+        $.ajax({
+            type: "GET",
+            url: apiURL + "/api/Game?gameCategory=sports&gameCount=5",
+            async: false,
+            success: function (data) {
+                $.each(data.Data, function (i, rec) {
+                    var gameImage = "<img class='gameImg' src='" + rec.banner_medium + "' style='margin:10px;'/>";
+                    $("#gameDiv").append(gameImage);
+                });
+                console.log(data);
+                //$scope.rowCollection = data.Data;
+                //$scope.loadDataTable();
+            },
+            error: function (data) {
+                //$.notify("Error Encountered: " + data.statusText, 'error');
+            }
+        });
+    };
+    $scope.getGameData();
+
+});
+
---
+++ GameZone.WEB/Scripts/Global.js
@@ -0,0 +1,42 @@
+﻿var gamezoneApp = angular.module('gamezoneApp', []);
+var apiURL = "http://localhost:62101";
+
+
+//Validate Form Controls
+function isFormValid(containerId) {
+    var isInValid = 0;
+    $('#' + containerId + ' input').each(function () {
+        try {
+            var id = $(this).attr('id'); if (id.indexOf('txt') >= 0) isInValid += (_isControlValid(id)) ? 0 : 1;
+            //document.writeln("id: " + id + " >isvalid: " + _isControlValid(id));
+        } catch (e) { }
+    });
+    $('#' + containerId + ' textarea').each(function () {
+        try {
+            var id = $(this).attr('id'); if (id.indexOf('txt') >= 0) isInValid += (_isControlValid(id)) ? 0 : 1;
+            //document.writeln("id: " + id + " >isvalid: " + _isControlValid(id));
+        } catch (e) { }
+    });
+    $('#' + containerId + ' select').each(function () {
+        try { var id = $(this).attr('id'); if (id.indexOf('sel') >= 0) isInValid += (_isControlValid(id)) ? 0 : 1; } catch (e) { }
+    });
+    //alert(containerID + " => nFailed: " + isInValid);
+    if (isInValid > 0) alertWarning('fill in the compulsory fields...');
+    return isInValid == 0 ? true : false;
+}
+
+//Get Angular App Variable
+function getNgScope() {
+    var appElement = document.querySelector('[ng-app=gamezoneApp]');
+    return angular.element(appElement).scope();
+}
+
+//Partially Harsh Parameter Value
+function partialHarsh(val) {
+    var valArray = val.split("").reverse(), retVal = "";
+    //Arabicize String (Read String from back)
+    for (var i = 0; i < valArray.length; i++) {
+        retVal += valArray[i] + "%0$";
+    }
+    return retVal;
+};
