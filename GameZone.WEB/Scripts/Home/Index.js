843d8ef21ca249a0dd06f0b1df355a02fca9075e
Aieysha <34538596+Aieysha@users.noreply.github.com>
2017-12-15 21:55:20 +0100

    Aisha's update
    my Ui changes

--- GameZone.WEB/Scripts/Home/Index.js
+++ GameZone.WEB/Scripts/Home/Index.js
@@ -15,7 +15,7 @@ gamezoneApp.controller('gamezoneCtrlr', function ($scope, $http) {
         
         $.ajax({
             type: "GET",
-            url: apiURL + "/api/Game?gameCategory=" + selectedCat + "&gameCount=9",
+            url: apiURL + "/api/Game?gameCategory=" + selectedCat + "&gameCount=60",
             async: true,
             success: function (data) {
                 var gameContent = "";
@@ -24,16 +24,16 @@ gamezoneApp.controller('gamezoneCtrlr', function ($scope, $http) {
 
                 $.each(data.Data, function (i, rec) {
                     if (rec.title != "What's My Icon?") {
-                        gameContent = "<div class='col-xs-12 col-sm-8 col-md-4 col-lg-4 isotopeSelector block " + selectedCat + "'>";
-                        gameContent = gameContent + "<div class='service-wrap hovereffect panel clearfix animate' data-animate='bounceIn' data-duration='1.0s' data-delay='0.2s'>";
+                        gameContent = "<div class='col-sm-2 isotopeSelector block " + selectedCat + "'>";
+                        gameContent = gameContent + "<div class='service-wrap hovereffect clearfix animate' data-animate='bounceIn' data-duration='1.0s' data-delay='0.2s'>";
                         gameContent = gameContent + "<a href='" + rec.url + "' class='game-link'>";
-                        gameContent = gameContent + "<p class='game-category hiddenPara'>" + selectedCat + "</p>";
-                        gameContent = gameContent + "<div class='longDescription hiddenPara'>" + rec.long_description + "</div>";
+                        //gameContent = gameContent + "<p class='game-category hiddenPara'>" + selectedCat + "</p>";
+                        //gameContent = gameContent + "<div class='longDescription hiddenPara'>" + rec.long_description + "</div>";
                         gameContent = gameContent + "<img src='" + rec.banner_medium + "' alt='" + rec.title + "' class='img-responsive' width='100%' height='186.45' max-width='294.98' max-height='187.7'/>";
-                        gameContent = gameContent + "<div class='overlay description'>";
-                        gameContent = gameContent + "<h3 class='game-title'>" + rec.title + "</h3>";
-                        gameContent = gameContent + "<p class='text-justify'>" + rec.short_description + "</p>";
-                        gameContent = gameContent + "</div></a></div></div>";
+                        //gameContent = gameContent + "<div class='overlay description'>";
+                        gameContent = gameContent + "<h5 class='game-title'>" + rec.title + "</h5>";
+                        //gameContent = gameContent + "<p class='text-justify'>" + rec.short_description + "</p>";
+                        gameContent = gameContent + "</a></div></div>";
                         $("#isotopeContainer").append(gameContent);
                         gameContent = "";
                     }
