4e6fb5de550c6567773078b5fa2f083e6483038e
Aieysha <34538596+Aieysha@users.noreply.github.com>
2018-01-29 12:57:57 +0100

    Aisha's modal fix
    the modal fix

--- GameZone.WEB/Content/js/site.js
+++ GameZone.WEB/Content/js/site.js
@@ -18,25 +18,25 @@ $(document).ready(function(){
     $('body').on('click', '#login-link', function (e) {
         e.preventDefault();
         $('#register-form').addClass('slideOutLeft');
-        $('#login-form').removeClass('hide').addClass('slideInRight');
         $('#login-header').removeClass('hide').addClass('fadeIn');
         $('#register-header').addClass('hide');       
         setTimeout(function () {
+            $('#login-form').addClass('slideInRight').removeClass('hide');
             $('#login-header').removeClass('fadeIn');            
             $('#register-form').addClass('hide').removeClass('slideOutLeft');
-        }, 500);
+        }, 250);
     });
 
     $('body').on('click', '#register-link', function (e) {
         e.preventDefault();
         $('#login-form').addClass('slideOutLeft');
-        $('#register-form').removeClass('hide').addClass('slideInRight');
         $('#login-header').addClass('hide ');
         $('#register-header').removeClass('hide').addClass('fadeIn');
         setTimeout(function () {
+            $('#register-form').addClass('slideInRight').removeClass('hide');
             $('#register-header').removeClass('fadeIn');
             $('#login-form').addClass('hide').removeClass('slideOutLeft');
-        }, 500);
+        }, 250);
     });
     //$(window).on('load',function(){
     //  $('#exampleModal').modal('show');
